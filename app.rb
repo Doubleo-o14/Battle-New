4109f88daf4e2368ad60430f746d5f1f3c13cbb4
88Andybird <anbird88@gmail.com>
2021-11-02 16:38:48 +0000

    added in a config.ru file and that we can run the app through

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
-require 'sinatra'
-require 'sinatra/reloader' if development?
+require 'sinatra/base'
+require 'sinatra/reloader'
 
-get '/battle' do
-  "battle"
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Hello Battle!'
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
